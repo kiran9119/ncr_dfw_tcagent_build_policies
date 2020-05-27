6cc8ff99d402b0da251ec904a61fd148bf9603b7
pk185085 <pk185085@ncr.com>
2020-05-27 08:29:04 +0530

    corrected the file layout issue

--- policies/ncr_dfw_tcagent_server50.rb
+++ policies/ncr_dfw_tcagent_server50.rb
@@ -5,8 +5,6 @@ default_source :supermarket, 'https://supermarket.hospitality.ncr.com'
 cookbook 'ncr_dfw_tcagent_build'
 
 run_list %w(
-    ncr_dfw_tcagent_build::default
-    cr_dfw_tcagent_build::alohapos
+  ncr_dfw_tcagent_build::default
+  ncr_dfw_tcagent_build::alohapos
 )
-
-
--- policies/ncr_dfw_tcagent_server57.rb
+++ policies/ncr_dfw_tcagent_server57.rb
@@ -5,7 +5,5 @@ default_source :supermarket, 'https://supermarket.hospitality.ncr.com'
 cookbook 'ncr_dfw_tcagent_build'
 
 run_list %w(
-    ncr_dfw_tcagent_build::default
+  ncr_dfw_tcagent_build::default
 )
-
-
